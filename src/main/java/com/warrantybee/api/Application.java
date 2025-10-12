77186435367c38a4451d1b455a4f7153f2a72baf
aroyofficial <aroy02072000@gmail.com>
2025-10-12 19:03:29 +0530

    Initial setup

---
+++ src/main/java/com/warrantybee/api/Application.java
@@ -0,0 +1,19 @@
+package com.warrantybee.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication (
+	exclude = {
+		SecurityAutoConfiguration.class
+	}
+)
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
