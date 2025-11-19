827676826fd2f343ad49e090330765823f44d8ad
aroyofficial <aroy02072000@gmail.com>
2025-11-19 20:07:37 +0530

    Get allowed origins from env #11

--- src/main/java/com/warrantybee/api/config/SecurityConfig.java
+++ src/main/java/com/warrantybee/api/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.warrantybee.api.config;
 
 import com.warrantybee.api.config.filters.AuthenticationFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +20,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${APP_WHITELISTED_ENDPOINTS:}")
+    private String _WHITELISTED_ENDPOINTS;
+    @Value("${APP_ALLOWED_ORIGINS:}")
+    private String _ALLOWED_ORIGINS;
     private final AuthenticationFilter _filter;
 
     public SecurityConfig(AuthenticationFilter filter) {
@@ -36,15 +41,7 @@ public class SecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .authorizeHttpRequests(auth ->
-                auth.requestMatchers(
-                    "/api/status",
-                    "/status.html",
-                    "/api/auth/login",
-                    "/api/auth/signup",
-                    "/api/auth/forgotpassword",
-                    "/api/auth/resetpassword",
-                    "/api/countries"
-                ).permitAll()
+                auth.requestMatchers(_WHITELISTED_ENDPOINTS.split(",")).permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(_filter, UsernamePasswordAuthenticationFilter.class);
@@ -55,12 +52,7 @@ public class SecurityConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://warrantybee-dev.com",
-                "https://warrantybee.netlify.app",
-                "https://warrantybeetest.netlify.app",
-                "https://warrantybeestage.netlify.app"
-        ));
+        configuration.setAllowedOrigins(List.of(_ALLOWED_ORIGINS.split(",")));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
